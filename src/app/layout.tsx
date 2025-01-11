427e4584fa14feedc22724a613dd8b95e9f0f16e
LakioLive <mr.lakio.sergii@gmail.com>
2025-01-11 21:06:28 +0100

    fix: Changed global styles file from CSS to SCSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./globals.scss";
 import Navbar from "@/components/layout/navbar/Navbar";
 import Footer from "@/components/layout/footer/Footer";
 
