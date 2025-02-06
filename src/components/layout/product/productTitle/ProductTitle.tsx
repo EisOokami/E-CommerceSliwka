d62046b052ea3f99f0c753ef5a237eb2ca5cafda
LakioLive <mr.lakio.sergii@gmail.com>
2025-02-06 18:45:34 +0100

    feat: Added ProductTitle component

---
+++ src/components/layout/product/productTitle/ProductTitle.tsx
@@ -0,0 +1,11 @@
+import { ProductTitleProps } from "./ProductTitle.interfaces";
+
+export default function ProductTitle({ title }: ProductTitleProps) {
+    return (
+        <div>
+            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
+                {title}
+            </h1>
+        </div>
+    );
+}
