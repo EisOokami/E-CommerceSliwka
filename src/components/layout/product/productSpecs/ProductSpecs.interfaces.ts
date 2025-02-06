5c68fefa6c85a8a3988bc793f9c23df9692eb0c0
LakioLive <mr.lakio.sergii@gmail.com>
2025-02-06 18:46:29 +0100

    feat: Added ProductSpecs component

---
+++ src/components/layout/product/productSpecs/ProductSpecs.interfaces.ts
@@ -0,0 +1,5 @@
+import { IconType } from "react-icons";
+
+export interface ProductSpecsProps {
+    specsData: { icon: IconType; title: string; spec: string }[];
+}
---
+++ src/components/layout/product/productSpecs/ProductSpecs.tsx
@@ -0,0 +1,20 @@
+import { ProductSpecsProps } from "./ProductSpecs.interfaces";
+
+export default function ProductSpecs({ specsData }: ProductSpecsProps) {
+    return (
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-2 md:gap-5">
+            {specsData.map((spec, i) => (
+                <div
+                    key={i}
+                    className="flex items-center gap-2 p-3 bg-gray-100 rounded-xl"
+                >
+                    <spec.icon className="text-2xl" />
+                    <div className="grid">
+                        <span className="text-gray-500">{spec.title}</span>
+                        <span>{spec.spec}</span>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
