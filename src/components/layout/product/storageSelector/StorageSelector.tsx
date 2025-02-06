96943b09dea3ce3c5a9959a9d72e39d78a9259f7
LakioLive <mr.lakio.sergii@gmail.com>
2025-02-06 18:46:13 +0100

    feat: Added StorageSelector component

---
+++ src/components/layout/product/storageSelector/StorageSelector.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState } from "react";
+import { StorageSelectorProps } from "./StorageSelector.interfaces";
+
+export default function StorageSelector({ storageData }: StorageSelectorProps) {
+    const [selectedStorage, setSelectedStorage] = useState(0);
+
+    return (
+        <div className="flex flex-wrap md:flex-nowrap justify-between items-center gap-2 md:gap-3">
+            {storageData.map((storage, i) => (
+                <button
+                    key={i}
+                    className={`text-center w-full py-3 border rounded-xl ${
+                        selectedStorage === i
+                            ? "border-black text-black"
+                            : "text-gray-600"
+                    }`}
+                    onClick={() => setSelectedStorage(i)}
+                >
+                    {storage}
+                </button>
+            ))}
+        </div>
+    );
+}
