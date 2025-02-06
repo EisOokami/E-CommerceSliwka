99e5c467339274d7751ea9b35b616e37e903ecfb
LakioLive <mr.lakio.sergii@gmail.com>
2025-02-06 18:45:58 +0100

    feat: Added ColorSelector component

---
+++ src/components/ui/colorSelector/ColorSelector.tsx
@@ -0,0 +1,18 @@
+import { ColorSelectorProps } from "./ColorSelector.interfaces";
+
+export default function ColorSelector({ colorsData }: ColorSelectorProps) {
+    return (
+        <div className="flex items-center gap-5">
+            <span>Select color: </span>
+            <div className="flex items-center gap-3">
+                {colorsData.map((color, i) => (
+                    <button
+                        key={i}
+                        className="w-9 h-9 rounded-full"
+                        style={{ backgroundColor: color }}
+                    ></button>
+                ))}
+            </div>
+        </div>
+    );
+}
