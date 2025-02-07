81b6fae9f1a2597a7e2640400279313351dd0173
LakioLive <mr.lakio.sergii@gmail.com>
2025-02-07 20:59:11 +0100

    feat: Added ProductPrice component

---
+++ src/components/layout/product/productPrice/ProductPrice.tsx
@@ -0,0 +1,19 @@
+import { ProductPriceProps } from "./ProductPrice.interfaces";
+
+export default function ProductPrice({
+    price,
+    discount = "",
+}: ProductPriceProps) {
+    return (
+        <div className="flex items-center gap-5">
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold">
+                {price}
+            </h1>
+            {discount && (
+                <h1 className="text-xl md:text-2xl lg:text-3xl text-gray-500 font-semibold line-through">
+                    {discount}
+                </h1>
+            )}
+        </div>
+    );
+}
