8a23a93bd582e06600efb09a86621a72c49327e3
LakioLive <mr.lakio.sergii@gmail.com>
2025-02-07 20:58:51 +0100

    feat: Added ProductInfo component

---
+++ src/components/layout/product/productInfo/ProductInfo.interfaces.ts
@@ -0,0 +1,5 @@
+import { IconType } from "react-icons";
+
+export interface ProductInfoProps {
+    infoData: { icon: IconType; title: string; info: string }[];
+}
---
+++ src/components/layout/product/productInfo/ProductInfo.tsx
@@ -0,0 +1,22 @@
+import { ProductInfoProps } from "./ProductInfo.interfaces";
+
+export default function ProductInfo({ infoData }: ProductInfoProps) {
+    return (
+        <div className="grid grid-cols-3 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {infoData.map((info, i) => (
+                <div
+                    key={i}
+                    className="grid md:flex justify-items-center content-start md:items-center gap-3"
+                >
+                    <div className="p-4 bg-gray-100 rounded-xl">
+                        <info.icon className="text-2xl text-gray-500" />
+                    </div>
+                    <div className="grid text-center md:text-left">
+                        <span className="text-gray-500">{info.title}</span>
+                        <span>{info.info}</span>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
