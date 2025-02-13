e26e956b7aae37c2e5e722914eb48d6370e74b99
LakioLive <mr.lakio.sergii@gmail.com>
2025-02-13 23:02:04 +0100

    feat: Added RatingSummary component

---
+++ src/components/ui/ratingSummary/RatingSummary.tsx
@@ -0,0 +1,91 @@
+import RatingStars from "@/components/ui/ratingStars/RatingStars";
+import { RatingSummaryProps } from "./RatingSummary.interfaces";
+
+export default function RatingSummary({ reviewsData }: RatingSummaryProps) {
+    const totalReviews = reviewsData.length;
+    const averageRating = +(
+        reviewsData.reduce(
+            (accumulator, currentValue) => currentValue.rating + accumulator,
+            0,
+        ) / totalReviews
+    ).toFixed(1);
+    const counterStarsData = [
+        {
+            star: "5 star",
+            countStars: reviewsData.reduce(
+                (accumulator, currentValue) =>
+                    (currentValue.rating === 5 ? 1 : 0) + accumulator,
+                0,
+            ),
+        },
+        {
+            star: "4 star",
+            countStars: reviewsData.reduce(
+                (accumulator, currentValue) =>
+                    (currentValue.rating === 4 ? 1 : 0) + accumulator,
+                0,
+            ),
+        },
+        {
+            star: "3 star",
+            countStars: reviewsData.reduce(
+                (accumulator, currentValue) =>
+                    (currentValue.rating === 3 ? 1 : 0) + accumulator,
+                0,
+            ),
+        },
+        {
+            star: "2 star",
+            countStars: reviewsData.reduce(
+                (accumulator, currentValue) =>
+                    (currentValue.rating === 2 ? 1 : 0) + accumulator,
+                0,
+            ),
+        },
+        {
+            star: "1 star",
+            countStars: reviewsData.reduce(
+                (accumulator, currentValue) =>
+                    (currentValue.rating === 1 ? 1 : 0) + accumulator,
+                0,
+            ),
+        },
+    ];
+
+    return (
+        <div className="grid md:flex items-center gap-20">
+            <div className="grid justify-items-center gap-3 md:w-min p-8 bg-gray-100 rounded-xl">
+                <h2 className="text-6xl font-semibold">{averageRating}</h2>
+                <span className="text-gray-500 font-medium">
+                    of {totalReviews} reviews
+                </span>
+                <div>
+                    <RatingStars count={averageRating} />
+                </div>
+            </div>
+            <div className="grid gap-5 w-full">
+                {counterStarsData.map((counterStars, i) => (
+                    <div key={i} className="flex items-center gap-5 w-full">
+                        <span className="w-28 text-xl font-semibold">
+                            {counterStars.star}
+                        </span>
+                        <div className="w-full bg-gray-200 rounded-full h-2.5">
+                            <div
+                                className="bg-yellow-500 h-2.5 rounded-full"
+                                style={{
+                                    width: `${
+                                        (counterStars.countStars * 100) /
+                                        totalReviews
+                                    }%`,
+                                }}
+                            ></div>
+                        </div>
+                        <span className="text-right text-xl text-gray-500">
+                            {counterStars.countStars}
+                        </span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
