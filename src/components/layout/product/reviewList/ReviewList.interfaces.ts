f868b70c5e9b8b54ecc243c3cc8766983fcd6318
LakioLive <mr.lakio.sergii@gmail.com>
2025-02-14 22:25:21 +0100

    feat: Added ReviewList component

---
+++ src/components/layout/product/reviewList/ReviewList.interfaces.ts
@@ -0,0 +1,10 @@
+export interface ReviewListProps {
+    reviewsData: {
+        avatar: string;
+        fullname: string;
+        rating: number;
+        comment: string;
+        publicationDate: string;
+        images: string[];
+    }[];
+}
---
+++ src/components/layout/product/reviewList/ReviewList.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState } from "react";
+import ReviewComment from "@/components/ui/reviewComment/ReviewComment";
+import Button from "@/components/ui/button/Button";
+import { ReviewListProps } from "./ReviewList.interfaces";
+
+export default function ReviewList({ reviewsData }: ReviewListProps) {
+    const [displayedData, setDisplayedData] = useState(reviewsData.slice(0, 3));
+    const [visibleItemCount, setVisibleItemCount] = useState(3);
+
+    const handleLoadMore = () => {
+        setVisibleItemCount((prevCount) => prevCount + 3);
+
+        setDisplayedData(reviewsData.slice(0, visibleItemCount + 3));
+    };
+
+    return (
+        <div className="grid gap-5">
+            {displayedData.map((review, i) => (
+                <ReviewComment key={i} review={review} />
+            ))}
+            {reviewsData.length !== displayedData.length && (
+                <div className="w-full text-center">
+                    <Button
+                        href=""
+                        text="View more"
+                        theme="dark"
+                        inline
+                        onClick={handleLoadMore}
+                    />
+                </div>
+            )}
+        </div>
+    );
+}
