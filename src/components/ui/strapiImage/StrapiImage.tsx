835364ee21a643fd16ae4bf5498f25d438ed550d
LakioLive <mr.lakio.sergii@gmail.com>
2025-03-01 20:13:27 +0100

    feat: Added StrapiImage component

---
+++ src/components/ui/strapiImage/StrapiImage.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { getStrapiMedia } from "@/lib/utils";
+import { StrapiImageProps } from "./StrapiImage.interfaces";
+
+export default function StrapiImage({
+    src,
+    alt,
+    height,
+    width,
+    className,
+}: Readonly<StrapiImageProps>) {
+    const imageUrl = getStrapiMedia(src);
+
+    if (!imageUrl) {
+        return null;
+    }
+
+    return (
+        <Image
+            src={imageUrl}
+            alt={alt}
+            height={height}
+            width={width}
+            className={className}
+        />
+    );
+}
