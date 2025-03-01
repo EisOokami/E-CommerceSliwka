d3c3117d4f573c332e7a9d0c7c8c949a643a31df
LakioLive <mr.lakio.sergii@gmail.com>
2025-03-01 23:29:44 +0100

    feat: Updated Logo component with additional props for link, src, and alt attributes

--- src/components/ui/logo/Logo.interfaces.ts
+++ src/components/ui/logo/Logo.interfaces.ts
@@ -1,4 +1,7 @@
 export interface LogoProps {
-    width: number | `${number}` | undefined;
-    height: number | `${number}` | undefined;
+    width: number | `${number}`;
+    height: number | `${number}`;
+    link: string;
+    src: string;
+    alt: string | null;
 }
--- src/components/ui/logo/Logo.tsx
+++ src/components/ui/logo/Logo.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
 import Link from "next/link";
 import { LogoProps } from "./Logo.interfaces";
+import StrapiImage from "../strapiImage/StrapiImage";
 
-export default function Logo({ width, height }: LogoProps) {
+export default function Logo({ width, height, link, src, alt }: LogoProps) {
     return (
-        <Link href="/">
-            <Image
-                src="/generallogoblue.png"
-                alt="logo"
+        <Link href={link ?? ""}>
+            <StrapiImage
+                src={src}
+                alt={alt ?? "logo"}
                 priority
                 width={width}
                 height={height}
