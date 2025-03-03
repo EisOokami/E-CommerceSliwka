2d790f9ed4090c8ae53facd3487de174d8d98012
LakioLive <mr.lakio.sergii@gmail.com>
2025-03-03 20:41:07 +0100

    build: Updated Nextjs configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        domains: ["127.0.0.1"],
+        // domains: ["127.0.0.1"],
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "127.0.0.1",
+                port: "1337",
+                pathname: "**",
+                search: "",
+            },
+        ],
     },
 };
 
