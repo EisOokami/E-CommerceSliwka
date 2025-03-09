7796fc67237601cab04c5dc7e1b5bd7f71800c97
LakioLive <mr.lakio.sergii@gmail.com>
2025-03-09 21:47:37 +0100

    feat: Added authService script

---
+++ src/data/services/authService.ts
@@ -0,0 +1,43 @@
+import { getStrapiURL } from "@/lib/utils";
+import { LoginUserProps, RegisterUserProps } from "./authService.interfaces";
+
+const baseUrl = getStrapiURL();
+
+export async function registerUserService(userData: RegisterUserProps) {
+    const url = new URL("/api/auth/local/register", baseUrl);
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ ...userData }),
+            cache: "no-cache",
+        });
+
+        return response.json();
+    } catch (error) {
+        console.error("Registration Service Error:", error);
+    }
+}
+
+export async function loginUserService(userData: LoginUserProps) {
+    const url = new URL("/api/auth/local", baseUrl);
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ ...userData }),
+            cache: "no-cache",
+        });
+
+        return response.json();
+    } catch (error) {
+        console.error("Login Service Error:", error);
+        throw error;
+    }
+}
