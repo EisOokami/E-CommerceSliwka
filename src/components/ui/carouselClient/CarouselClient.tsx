f2a84508cc217c72e109e1835490331b5437d290
LakioLive <mr.lakio.sergii@gmail.com>
2025-03-31 23:08:03 +0200

    feat: Updated CarouselClient component

--- src/components/ui/carouselClient/CarouselClient.tsx
+++ src/components/ui/carouselClient/CarouselClient.tsx
@@ -18,29 +18,39 @@ export default function CarouselClient({
     return (
         <Splide options={options} hasTrack={false}>
             <SplideTrack>
-                {productsData.map((product, i) => (
-                    <SplideSlide key={i}>
-                        <div className="grid gap-5 p-8 bg-gray-100">
-                            <Image
-                                src={product.src}
-                                alt={product.alt}
-                                width={width}
-                                height={height}
-                                className="justify-self-center"
-                            />
-                            <h4 className="text-2xl md:text-xl xl:text-4xl">
-                                {product.name}
-                            </h4>
-                            <span className="text-gray-500">
-                                {product.descr}
-                            </span>
-                            <Button
-                                href={product.href}
-                                theme="dark"
-                                text="Shop now"
-                                inline
-                                isLink
-                            />
+                {productsData.map((product) => (
+                    <SplideSlide key={product.documentId}>
+                        <div className="flex flex-col justify-between gap-5 p-8 bg-gray-100 h-full">
+                            <div className="justify-self-center self-center grid place-content-center h-1/2">
+                                <Image
+                                    src={`${process.env.NEXT_PUBLIC_DB_URL}${product.image.url}`}
+                                    alt={
+                                        product.image.alternativeText ??
+                                        product.name
+                                    }
+                                    width={width}
+                                    height={height}
+                                    className="h-fit"
+                                />
+                            </div>
+                            <div className="grid gap-3 h-1/2">
+                                <div className="grid content-start gap-5">
+                                    <h4 className="text-2xl md:text-xl xl:text-4xl">
+                                        {product.name}
+                                    </h4>
+                                    <p className="text-gray-500 text-pretty">
+                                        {product.description}
+                                    </p>
+                                </div>
+                                <Button
+                                    href={`/catalog/${product.id}`}
+                                    theme="dark"
+                                    text="Shop now"
+                                    className="self-end"
+                                    inline
+                                    isLink
+                                />
+                            </div>
                         </div>
                     </SplideSlide>
                 ))}
