6d2fb74e3163077167620f6bcfc98344c8d4ae94
LakioLive <mr.lakio.sergii@gmail.com>
2025-04-01 22:05:35 +0200

    fix: Fixed a bug where displayed incorrect pricing with discounts in ProductPrice component

--- src/components/layout/product/productPrice/ProductPrice.tsx
+++ src/components/layout/product/productPrice/ProductPrice.tsx
@@ -8,11 +8,11 @@ export default function ProductPrice({
     return (
         <div className="flex items-center gap-5">
             <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold">
-                ${price}
+                ${isDiscount ? discount : price}
             </h1>
             {isDiscount ? (
                 <h1 className="text-xl md:text-2xl lg:text-3xl text-gray-500 font-semibold line-through">
-                    ${discount}
+                    ${price}
                 </h1>
             ) : null}
         </div>
