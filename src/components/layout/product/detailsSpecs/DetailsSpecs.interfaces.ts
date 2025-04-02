2d2653913db3056a469bba09b254c15eab63640d
LakioLive <mr.lakio.sergii@gmail.com>
2025-04-02 22:45:26 +0200

    feat: Updated DetailsSpecs component

--- src/components/layout/product/detailsSpecs/DetailsSpecs.interfaces.ts
+++ src/components/layout/product/detailsSpecs/DetailsSpecs.interfaces.ts
@@ -1,8 +1,19 @@
-export interface ISpecs {
+export interface ISpecification {
+    id: number;
+    item: string;
+}
+export interface ISpecifications {
+    id: number;
     name: string;
-    value: string | string[];
+    specifications: ISpecification[];
+}
+
+export interface IDetailedSpecifications {
+    id: number;
+    name: string;
+    specifications: ISpecifications[];
 }
 
 export interface DetailsSpecsProps {
-    specsData: { title: string; specs: ISpecs[] }[];
+    specsData: IDetailedSpecifications[];
 }
--- src/components/layout/product/detailsSpecs/DetailsSpecs.tsx
+++ src/components/layout/product/detailsSpecs/DetailsSpecs.tsx
@@ -11,45 +11,44 @@ export default function DetailsSpecs({
 
     return (
         <div className="grid gap-8">
-            {specsData.map(({ title, specs }, i) => {
-                return (
+            {specsData.map(
+                (specData, i) =>
                     (showMore || i < 2) && (
-                        <div key={i} className="grid gap-3">
+                        <div key={specData.id} className="grid gap-3">
                             <h6 className="text-lg md:text-2xl font-medium">
-                                {title}
+                                {specData.name}
                             </h6>
                             <div className="grid gap-5">
-                                {specs.map(({ name, value }, j) => (
-                                    <article
-                                        key={j}
-                                        className="flex justify-between items-start pb-3 border-b"
-                                    >
-                                        <span className="w-1/3 font-medium">
-                                            {name}
-                                        </span>
-                                        <div className="grid gap-2 w-2/3 text-right">
-                                            {Array.isArray(value) ? (
-                                                value.map((spec, k) => (
-                                                    <span
-                                                        key={k}
-                                                        className="break-all"
-                                                    >
-                                                        {spec}
-                                                    </span>
-                                                ))
-                                            ) : (
-                                                <span className="break-words">
-                                                    {value}
-                                                </span>
-                                            )}
-                                        </div>
-                                    </article>
-                                ))}
+                                {specData.specifications.map(
+                                    (detailedSpecification) => (
+                                        <article
+                                            key={detailedSpecification.id}
+                                            className="flex justify-between items-start pb-3 border-b"
+                                        >
+                                            <span className="w-1/3 font-medium">
+                                                {detailedSpecification.name}
+                                            </span>
+                                            <div className="grid gap-2 w-2/3 text-right">
+                                                {detailedSpecification.specifications.map(
+                                                    (specification) => (
+                                                        <span
+                                                            key={
+                                                                specification.id
+                                                            }
+                                                            className="break-all"
+                                                        >
+                                                            {specification.item}
+                                                        </span>
+                                                    ),
+                                                )}
+                                            </div>
+                                        </article>
+                                    ),
+                                )}
                             </div>
                         </div>
-                    )
-                );
-            })}
+                    ),
+            )}
             <div className="w-full text-center">
                 <Button
                     text={showMore ? "View less" : "View more"}
