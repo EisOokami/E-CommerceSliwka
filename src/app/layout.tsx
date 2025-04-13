3eacfcd685b0c434202effe1e8ff3e222afa6901
LakioLive <mr.lakio.sergii@gmail.com>
2025-04-13 21:36:53 +0200

    feat: Updated home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,60 @@
 import type { Metadata } from "next";
+import { getGlobalData, getGlobalPageMetadata } from "@/data/loaders";
+import { getUserMeLoader } from "@/data/services/getUserMeLoader";
 import "./globals.scss";
 import Header from "@/components/layout/Header/Header";
 import Footer from "@/components/layout/footer/Footer";
 
-export const metadata: Metadata = {
-    title: "E-Commerce Sliwka",
-    description: "",
-    icons: "/icon.ico",
-};
+export async function generateMetadata(): Promise<Metadata> {
+    const metadata = await getGlobalPageMetadata();
 
-export default function RootLayout({
+    return {
+        title: metadata?.data?.title ?? "E-Commerce Sliwka",
+        description:
+            metadata?.data?.description ??
+            "Buy the latest products at great prices. Fast delivery only from us!",
+        icons: "/icon.ico",
+        keywords: [
+            "gadgets",
+            "smartphones",
+            "laptops",
+            "accessories",
+            "buy online",
+            "nextjs",
+            "e-commerce",
+            "e-commerce sliwka",
+            "lakiolive",
+            "lakio",
+            "fast delivery",
+        ],
+        authors: [{ name: "LakioLive", url: "https://github.com/LakioLive" }],
+        robots: "index, follow",
+        other: {
+            "application/ld+json": JSON.stringify({
+                "@context": "https://schema.org",
+                "@type": "Organization",
+                name: "E-Commerce Sliwka",
+                //   "url": "https://example.com",
+                //   "logo": "https://example.com/logo.png",
+            }),
+        },
+    };
+}
+
+export default async function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const globalData = await getGlobalData();
+    const user = await getUserMeLoader();
+
     return (
         <html lang="en">
             <body>
-                <Header />
+                <Header data={globalData.data.header} isUserSingIn={user.ok} />
                 {children}
-                <Footer />
+                <Footer data={globalData.data.footer} />
             </body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
-import DiscountsShowcase from "@/components/layout/home/discountsShowcase/DiscountsShowcase";
+import { getHomePageData } from "@/data/loaders";
+
 import Hero from "@/components/layout/home/hero/Hero";
+import FeatureProducts from "@/components/layout/home/featureProducts/FeatureProducts";
 import Products from "@/components/layout/home/products/Products";
 import ProductsCarousel from "@/components/layout/home/productsCarousel/ProductsCarousel";
+import DiscountsShowcase from "@/components/layout/home/discountsShowcase/DiscountsShowcase";
 
-export default function Home() {
+export default async function HomePage() {
+    const strapiData = await getHomePageData();
+    const { blocks } = strapiData?.data || [];
+
     return (
         <main>
-            <Hero />
-            <Products />
-            <ProductsCarousel />
-            <DiscountsShowcase />
+            <Hero data={blocks[0]} />
+            <FeatureProducts data={blocks[1]} />
+            <Products data={blocks[4]} />
+            <ProductsCarousel data={blocks[3]} />
+            <DiscountsShowcase data={blocks[2]} />
         </main>
     );
 }
