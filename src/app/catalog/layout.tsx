e90e97d9f0e12b488fff0a39db5d161089376bfc
LakioLive <mr.lakio.sergii@gmail.com>
2025-04-13 21:29:13 +0200

    feat: Updated catalog page

--- src/app/catalog/layout.tsx
+++ src/app/catalog/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Breadcrumb from "@/components/ui/breadcrumb/Breadcrumb";
 
 export const metadata: Metadata = {
     title: "E-Commerce Sliwka",
@@ -13,11 +12,6 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <main className="relative grid gap-10 py-6 md:py-10">
-            <section className="container mx-auto px-3 md:px-5">
-                <Breadcrumb />
-            </section>
-            {children}
-        </main>
+        <main className="relative grid gap-10 py-6 md:py-10">{children}</main>
     );
 }
--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,21 +1,38 @@
-import AccordionContainer from "@/components/layout/catalog/accordionContainer/AccordionContainer";
-import Products from "@/components/layout/catalog/products/Products";
-import RatingSelect from "@/components/ui/ratingSelect/RatingSelect";
+"use client";
+
+import { Suspense, useState } from "react";
+
+import Breadcrumb from "@/components/ui/breadcrumb/Breadcrumb";
+import Catalog from "@/components/layout/catalog/Catalog";
 import Pagination from "@/components/ui/pagination/Pagination";
 
+const limit = 8;
+
 export default function CatalogPage() {
+    const [totalPages, setTotalPages] = useState<number>(1);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [productsCount, setProductsCount] = useState<number>(0);
+
     return (
         <>
-            <section className="grid md:flex md:items-center gap-5 container mx-auto px-3 md:px-5">
-                <div className="md:self-start flex justify-between items-center gap-5 md:block md:w-1/3">
-                    <AccordionContainer />
-                    <div className="block md:hidden w-full">
-                        <RatingSelect />
-                    </div>
-                </div>
-                <Products />
+            <section className="container mx-auto px-3 md:px-5">
+                <Breadcrumb />
             </section>
-            <Pagination />
+            <Suspense fallback={<p>Loading products...</p>}>
+                <Catalog
+                    limit={limit}
+                    totalPages={totalPages}
+                    setTotalPages={setTotalPages}
+                    currentPage={currentPage}
+                    productsCount={productsCount}
+                    setProductsCount={setProductsCount}
+                />
+            </Suspense>
+            <Pagination
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                totalPages={totalPages}
+            />
         </>
     );
 }
