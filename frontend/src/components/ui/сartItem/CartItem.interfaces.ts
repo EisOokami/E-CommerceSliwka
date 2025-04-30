4d88164e9da8d4eb9fb74befe550f84ed9735d72
LakioLive <mr.lakio.sergii@gmail.com>
2025-04-30 22:34:40 +0200

    feat: Updated CartItem component

--- frontend/src/components/ui/сartItem/CartItem.interfaces.ts
+++ frontend/src/components/ui/сartItem/CartItem.interfaces.ts
@@ -1,9 +1,14 @@
+import { IStore } from "@/interfaces/interfaces";
+
 export interface CartItemProps {
     cartItem: {
-        image: string;
-        name: string;
-        productId: string;
-        count: number;
-        price: number;
+        id: number;
+        documentId: number;
+        quantity: number;
+        store: IStore;
+        user: {
+            documentId: number;
+            id: number;
+        };
     };
 }
--- frontend/src/components/ui/сartItem/CartItem.tsx
+++ frontend/src/components/ui/сartItem/CartItem.tsx
@@ -7,8 +7,10 @@ export default function CartItem({ cartItem }: Readonly<CartItemProps>) {
     return (
         <div className="flex items-center gap-6 w-full py-8 first:border-none border-t">
             <Image
-                src={cartItem.image}
-                alt={cartItem.name}
+                src={`${process.env.NEXT_PUBLIC_DB_URL}${cartItem.store.image.url}`}
+                alt={
+                    cartItem.store.image.alternativeText ?? cartItem.store.name
+                }
                 width={100}
                 height={100}
                 className="w-20 lg:w-auto h-20 lg:h-auto"
@@ -16,10 +18,10 @@ export default function CartItem({ cartItem }: Readonly<CartItemProps>) {
             <div className="grid xl:flex items-center gap-2 w-full">
                 <div className="md:flex-1 grid gap-2">
                     <h6 className="text-base lg:text-xl text-pretty font-medium">
-                        {cartItem.name}
+                        {cartItem.store.name}
                     </h6>
                     <span className="text-sm lg:text-base">
-                        {cartItem.productId}
+                        #{cartItem.store.documentId}
                     </span>
                 </div>
                 <div className="flex justify-start items-center gap-2 md:gap-6">
@@ -28,14 +30,17 @@ export default function CartItem({ cartItem }: Readonly<CartItemProps>) {
                             <FaMinus />
                         </button>
                         <span className="px-2 md:px-4 py-0.5 md:py-1 border rounded">
-                            {cartItem.count}
+                            {cartItem.quantity}
                         </span>
                         <button>
                             <FaPlus />
                         </button>
                     </div>
                     <h5 className="text-xl lg:text-2xl font-medium">
-                        ${cartItem.price}
+                        $
+                        {cartItem.store.isDiscount
+                            ? cartItem.store.discountedPrice
+                            : cartItem.store.price}
                     </h5>
                     <button>
                         <IoCloseOutline className="text-3xl" />
