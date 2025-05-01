84efd0ad68d02e8fa88ca18ffc14c29a4cc8b262
LakioLive <mr.lakio.sergii@gmail.com>
2025-05-01 23:59:31 +0200

    fix: Deleted console.dir in authActions

--- frontend/src/data/actions/authActions.ts
+++ frontend/src/data/actions/authActions.ts
@@ -117,8 +117,6 @@ export async function loginUserAction(prevState: any, formData: FormData) {
         };
     }
 
-    console.log(responseData, "responseData");
-
     const cookieStore = await cookies();
     cookieStore.set("jwt", responseData.jwt, config);
 
