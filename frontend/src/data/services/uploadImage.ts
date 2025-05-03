3591e00ad384eb5e595d6065b35e32f72aabc4e1
LakioLive <mr.lakio.sergii@gmail.com>
2025-05-03 22:59:16 +0200

    feat: Added uploadImage script

---
+++ frontend/src/data/services/uploadImage.ts
@@ -0,0 +1,30 @@
+import { getStrapiURL } from "@/lib/utils";
+import { getAuthToken } from "./getToken";
+
+export async function uploadImagesToStrapi(images: File[]) {
+    const baseUrl = getStrapiURL();
+    const authToken = await getAuthToken();
+    const url = new URL("/api/upload", baseUrl);
+
+    if (!authToken) throw new Error("No auth token found");
+
+    const uploadFormData = new FormData();
+    images.forEach((file) => uploadFormData.append("files", file));
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${authToken}`,
+            },
+            body: uploadFormData,
+        });
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.log("error", error);
+        throw error;
+    }
+}
