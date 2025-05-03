59923bd263be38da1af869916409989412d690f4
LakioLive <mr.lakio.sergii@gmail.com>
2025-05-03 23:00:54 +0200

    feat: Added WarningMsg component

---
+++ frontend/src/components/ui/warningMsg/WarningMsg.tsx
@@ -0,0 +1,11 @@
+import { IoWarningOutline } from "react-icons/io5";
+import { WarningMsgProps } from "./WarningMsg.interfaces";
+
+export default function WarningMsg({ message }: WarningMsgProps) {
+    return (
+        <div className="flex items-center gap-2 p-5 text-white bg-yellow-500 rounded-xl">
+            <IoWarningOutline className="text-4xl" />
+            <p className="text-2xl">{message}</p>
+        </div>
+    );
+}
