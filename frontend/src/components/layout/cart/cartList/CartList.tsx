39d1c71494a1ac141c53ae27aa5bf124fc3cd757
LakioLive <mr.lakio.sergii@gmail.com>
2025-05-14 20:27:26 +0200

    feat: Added message when there are no products in CartList component

--- frontend/src/components/layout/cart/cartList/CartList.tsx
+++ frontend/src/components/layout/cart/cartList/CartList.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
 import { CartListProps } from "./CartList.interfaces";
 
 import CartItem from "@/components/ui/сartItem/CartItem";
@@ -18,6 +19,21 @@ export default function CartList({ cartItemsData }: Readonly<CartListProps>) {
                         setDeletedProducts={setDeletedProducts}
                     />
                 ) : null,
+            )}
+            {(!cartItemsData.length ||
+                deletedProducts.length === cartItemsData.length) && (
+                <div className="grid place-items-center gap-2 w-full mt-5">
+                    <Image
+                        src="/no-data.svg"
+                        alt="no-data"
+                        width={300}
+                        height={300}
+                    />
+                    <h1 className="text-center text-5xl text-gray-500 font-medium">
+                        Sorry... <br />
+                        no result found
+                    </h1>
+                </div>
             )}
         </div>
     );
