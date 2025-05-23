bc28037a6e6dce3448f0183f9362f26a2012358c
LakioLive <mr.lakio.sergii@gmail.com>
2025-05-23 23:16:23 +0200

    fix: Fixed responsive design in WarningMsg component

--- frontend/src/components/ui/warningMsg/WarningMsg.tsx
+++ frontend/src/components/ui/warningMsg/WarningMsg.tsx
@@ -5,7 +5,7 @@ export default function WarningMsg({ message }: WarningMsgProps) {
     return (
         <div className="flex items-center gap-2 p-5 text-white bg-yellow-500 rounded-xl">
             <IoWarningOutline className="text-4xl" />
-            <p className="text-2xl">{message}</p>
+            <p className="text-xl md:text-2xl">{message}</p>
         </div>
     );
 }
