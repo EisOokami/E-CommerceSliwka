8b1c3b9f0eb112727aa0e43f8f0275c7a983c026
LakioLive <mr.lakio.sergii@gmail.com>
2025-05-28 21:30:08 +0200

    feat: Added usePrevNextButtons hook

---
+++ frontend/src/hooks/UsePrevNextButtons.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { EmblaCarouselType } from "embla-carousel";
+
+interface IUsePrevNextButtons {
+    prevBtnDisabled: boolean;
+    nextBtnDisabled: boolean;
+    onPrevButtonClick: () => void;
+    onNextButtonClick: () => void;
+}
+
+export function usePrevNextButtons(
+    emblaApi: EmblaCarouselType | undefined,
+): IUsePrevNextButtons {
+    const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
+    const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+
+    const onPrevButtonClick = useCallback(() => {
+        if (!emblaApi) {
+            return;
+        }
+
+        emblaApi.scrollPrev();
+    }, [emblaApi]);
+
+    const onNextButtonClick = useCallback(() => {
+        if (!emblaApi) {
+            return;
+        }
+
+        emblaApi.scrollNext();
+    }, [emblaApi]);
+
+    const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+        setPrevBtnDisabled(!emblaApi.canScrollPrev());
+        setNextBtnDisabled(!emblaApi.canScrollNext());
+    }, []);
+
+    useEffect(() => {
+        if (!emblaApi) {
+            return;
+        }
+
+        onSelect(emblaApi);
+        emblaApi.on("reInit", onSelect).on("select", onSelect);
+    }, [emblaApi, onSelect]);
+
+    return {
+        prevBtnDisabled,
+        nextBtnDisabled,
+        onPrevButtonClick,
+        onNextButtonClick,
+    };
+}
