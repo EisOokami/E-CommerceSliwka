2f8d94fd5ecdd62d4227d6b73f1765b64823e9ef
LakioLive <mr.lakio.sergii@gmail.com>
2025-05-29 23:39:11 +0200

    feat: Updated ReviewList component

--- frontend/src/components/layout/product/reviewList/ReviewList.tsx
+++ frontend/src/components/layout/product/reviewList/ReviewList.tsx
@@ -6,7 +6,10 @@ import { ReviewListProps } from "./ReviewList.interfaces";
 import ReviewComment from "@/components/ui/reviewComment/ReviewComment";
 import Button from "@/components/ui/button/Button";
 
-export default function ReviewList({ reviewsData }: Readonly<ReviewListProps>) {
+export default function ReviewList({
+    reviewsData,
+    user,
+}: Readonly<ReviewListProps>) {
     const [displayedData, setDisplayedData] = useState(reviewsData.slice(0, 3));
     const [visibleItemCount, setVisibleItemCount] = useState(3);
 
@@ -19,7 +22,7 @@ export default function ReviewList({ reviewsData }: Readonly<ReviewListProps>) {
     return (
         <div className="grid gap-5">
             {displayedData.map((review) => (
-                <ReviewComment key={review.id} review={review} />
+                <ReviewComment key={review.id} review={review} user={user} />
             ))}
             {reviewsData.length !== displayedData.length && (
                 <div className="w-full text-center">
