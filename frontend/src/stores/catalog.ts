2c446abb72264d3a0b604b234a1d5688e6e44d8a
LakioLive <mr.lakio.sergii@gmail.com>
2025-06-07 21:05:51 +0200

    feat: Added catalog store

---
+++ frontend/src/stores/catalog.ts
@@ -0,0 +1,45 @@
+import { IProduct } from "@/interfaces/interfaces";
+import { create } from "zustand";
+
+interface ICatalogStore {
+    productsData: IProduct[];
+    setProductsData: (newProductsData: IProduct[]) => void;
+    totalPages: number;
+    setTotalPages: (newTotalPages: number) => void;
+    currentPage: number;
+    setCurrentPage: (newCurrentPage: number) => void;
+    productsCount: number;
+    setProductsCount: (newProductsCount: number) => void;
+    productRating: string;
+    setProductRating: (newProductRating: string) => void;
+}
+
+const useCatalogStore = create<ICatalogStore>((set) => ({
+    productsData: [],
+    setProductsData: (newProductsData) =>
+        set(() => ({
+            productsData: newProductsData,
+        })),
+    totalPages: 1,
+    setTotalPages: (newTotalPages) =>
+        set(() => ({
+            totalPages: newTotalPages,
+        })),
+    currentPage: 1,
+    setCurrentPage: (newCurrentPage) =>
+        set(() => ({
+            currentPage: newCurrentPage,
+        })),
+    productsCount: 0,
+    setProductsCount: (newProductsCount) =>
+        set(() => ({
+            productsCount: newProductsCount,
+        })),
+    productRating: "all",
+    setProductRating: (newProductRating) =>
+        set(() => ({
+            productRating: newProductRating,
+        })),
+}));
+
+export default useCatalogStore;
