776d83e257b3ea1fe2d40a6869de7381b16bb829
LakioLive <mr.lakio.sergii@gmail.com>
2025-06-10 21:15:15 +0200

    style: Fixed white-spacing in ProfileButton component

--- frontend/src/components/ui/profileButton/ProfileButton.tsx
+++ frontend/src/components/ui/profileButton/ProfileButton.tsx
@@ -24,7 +24,7 @@ export default function ProfileButton({
     };
 
     return (
-        <div className="relative ">
+        <div className="relative">
             {!isOpen && (
                 <Tooltip message={text}>
                     <button
