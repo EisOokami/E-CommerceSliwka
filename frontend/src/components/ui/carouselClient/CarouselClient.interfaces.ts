c5a4d2614598732b6750c12098dbc55328fdddae
LakioLive <mr.lakio.sergii@gmail.com>
2025-06-10 21:16:48 +0200

    feat: Deleted CarouselClient component

--- frontend/src/components/ui/carouselClient/CarouselClient.interfaces.ts
+++
@@ -1,8 +0,0 @@
-import { IProduct } from "@/interfaces/interfaces";
-
-export interface CarouselClientProps {
-    productsData: IProduct[];
-    options: unknown;
-    width: number | `${number}` | undefined;
-    height: number | `${number}` | undefined;
-}
