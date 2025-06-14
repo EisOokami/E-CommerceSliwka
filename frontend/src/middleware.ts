231c3ef1d2eb26223f26242e3531e55613067eea
LakioLive <mr.lakio.sergii@gmail.com>
2025-06-14 23:39:30 +0200

    feat: Added protect routes for authentication accounts in middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -3,17 +3,26 @@ import type { NextRequest } from "next/server";
 import { getUserMeLoader } from "./data/services/getUserMeLoader";
 
 const protectedRoutes = ["/cart", "/profile", "/wishlist"];
+const protectedAuthRoutes = ["/signIn", "/signUp"];
 
 function isProtectedRoute(path: string): boolean {
     return protectedRoutes.some((route) => path.startsWith(route));
+}
+
+function isProtectedAuthRoute(path: string): boolean {
+    return protectedAuthRoutes.some((route) => path.startsWith(route));
 }
 
 export async function middleware(request: NextRequest) {
     const user = await getUserMeLoader();
     const currentPath = request.nextUrl.pathname;
 
-    if (isProtectedRoute(currentPath) && user.ok === false) {
+    if (isProtectedRoute(currentPath) && !user.ok) {
         return NextResponse.redirect(new URL("/signIn", request.url));
+    }
+
+    if (isProtectedAuthRoute(currentPath) && user.ok) {
+        return NextResponse.redirect(new URL("/", request.url));
     }
 
     return NextResponse.next();
