7b48c7ed4e647a60c91063a9c04ab961daaf0dfa
LakioLive <mr.lakio.sergii@gmail.com>
2025-06-19 22:06:25 +0200

    fix: Added ProductsCarouselSection instead of ProductsCarousel in HomePage component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import { getHomePageData } from "@/data/loaders";
 import Hero from "@/components/layout/home/hero/Hero";
 import FeatureProducts from "@/components/layout/home/featureProducts/FeatureProducts";
 import Products from "@/components/layout/home/products/Products";
-import ProductsCarousel from "@/components/layout/home/productsCarousel/ProductsCarousel";
+import ProductsCarouselSection from "@/components/layout/home/productsCarouselSection/ProductsCarouselSection";
 import DiscountsShowcase from "@/components/layout/home/discountsShowcase/DiscountsShowcase";
 
 export default async function HomePage() {
@@ -15,7 +15,7 @@ export default async function HomePage() {
             <Hero data={blocks[0]} />
             <FeatureProducts data={blocks[1]} />
             <Products data={blocks[4]} />
-            <ProductsCarousel data={blocks[3]} />
+            <ProductsCarouselSection data={blocks[3]} />
             <DiscountsShowcase data={blocks[2]} />
         </main>
     );
