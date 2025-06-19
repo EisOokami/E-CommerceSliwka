9f4663811accf1977fda672c209fc25573cc73b6
LakioLive <mr.lakio.sergii@gmail.com>
2025-06-19 22:03:28 +0200

    feat: Updated ProductPrice component

--- frontend/src/components/layout/product/productPrice/ProductPrice.tsx
+++ frontend/src/components/layout/product/productPrice/ProductPrice.tsx
@@ -1,18 +1,37 @@
+"use client";
+
+import useProductStore from "@/stores/product";
 import { ProductPriceProps } from "./ProductPrice.interfaces";
 
 export default function ProductPrice({
     price,
     isDiscount,
     discount,
+    colorsData,
+    optionsData,
 }: Readonly<ProductPriceProps>) {
+    const selectedOption = useProductStore((state) => state.selectedOption);
+    const selectedColor = useProductStore((state) => state.selectedColor);
+    const optionPrice =
+        optionsData && optionsData.length
+            ? optionsData[selectedOption].priceDifference
+            : 0;
+    const colorPrice =
+        colorsData && colorsData.length
+            ? colorsData[selectedColor].priceDifference
+            : 0;
+
     return (
         <div className="flex items-center gap-5">
             <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold">
-                ${isDiscount ? discount : price}
+                $
+                {isDiscount && discount
+                    ? discount + colorPrice + optionPrice
+                    : price + colorPrice + optionPrice}
             </h1>
             {isDiscount ? (
                 <h1 className="text-xl md:text-2xl lg:text-3xl text-gray-500 font-semibold line-through">
-                    ${price}
+                    ${price + colorPrice + optionPrice}
                 </h1>
             ) : null}
         </div>
