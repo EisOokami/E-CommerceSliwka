ba85645fc5fa08985e9650d338fdae8c9a09edd1
LakioLive <mr.lakio.sergii@gmail.com>
2025-06-24 23:13:28 +0200

    feat: Updated handleDeleteProductFromCart logic, updated product price validation, updated image styles and added an option, color name to product name in CartItem component

--- frontend/src/components/ui/сartItem/CartItem.interfaces.ts
+++ frontend/src/components/ui/сartItem/CartItem.interfaces.ts
@@ -1,7 +1,5 @@
-import { Dispatch, SetStateAction } from "react";
 import { ICart } from "@/interfaces/interfaces";
 
 export interface CartItemProps {
     cartItem: ICart;
-    setDeletedProducts: Dispatch<SetStateAction<string[]>>;
 }
--- frontend/src/components/ui/сartItem/CartItem.tsx
+++ frontend/src/components/ui/сartItem/CartItem.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import useGlobalStore from "@/stores/global";
+import useCartStore from "@/stores/cart";
 import Image from "next/image";
 import { IoCloseOutline } from "react-icons/io5";
 import { FaPlus, FaMinus } from "react-icons/fa6";
@@ -10,11 +12,16 @@ import {
     removeQuantityToProductAction,
 } from "@/data/actions/cartActions";
 import { CartItemProps } from "./CartItem.interfaces";
+import { getProductsInCartCount } from "@/data/loaders";
 
-export default function CartItem({
-    cartItem,
-    setDeletedProducts,
-}: Readonly<CartItemProps>) {
+export default function CartItem({ cartItem }: Readonly<CartItemProps>) {
+    const setProductsInCartCount = useGlobalStore(
+        (state) => state.setProductsInCartCount,
+    );
+    const deletedProducts = useCartStore((state) => state.deletedProducts);
+    const setDeletedProducts = useCartStore(
+        (state) => state.setDeletedProducts,
+    );
     const [productQuantity, setProductQuantity] = useState<number>(
         cartItem.quantity,
     );
@@ -35,10 +42,19 @@ export default function CartItem({
         });
     };
 
-    const handleDeleteProductFromCart = () => {
+    const handleDeleteProductFromCart = async () => {
+        setDeletedProducts([...deletedProducts, cartItem.documentId]);
         deleteProductFromCartAction(cartItem.product.documentId);
-        setDeletedProducts((prevState) => [...prevState, cartItem.documentId]);
+
+        const updatedProductsInCartCount = await getProductsInCartCount();
+
+        setProductsInCartCount(updatedProductsInCartCount);
     };
+
+    const optionPrice = cartItem.option ? cartItem.option.priceDifference : 0;
+    const colorPrice = cartItem.color ? cartItem.color.priceDifference : 0;
+    const optionValue = cartItem.option ? cartItem.option.value : "";
+    const colorName = cartItem.color ? cartItem.color.colorName : "";
 
     return (
         <section className="flex items-center gap-6 w-full min-h-48 py-8 first:border-none border-t">
@@ -50,12 +66,14 @@ export default function CartItem({
                 }
                 width={100}
                 height={100}
-                className="w-20 lg:w-auto h-20 lg:h-auto"
+                className="w-20 lg:w-auto h-20 lg:h-auto object-contain"
             />
             <div className="grid xl:flex items-center gap-2 w-full">
                 <div className="md:flex-1 grid gap-2">
                     <h6 className="text-base lg:text-xl text-pretty font-medium">
                         {cartItem.product.name}
+                        {cartItem.option || cartItem.color ? " | " : " "}
+                        {optionValue} {colorName}
                     </h6>
                     <span className="text-sm lg:text-base">
                         #{cartItem.product.documentId}
@@ -75,9 +93,12 @@ export default function CartItem({
                     </div>
                     <h5 className="text-xl lg:text-2xl font-medium">
                         $
-                        {cartItem.product.isDiscount
-                            ? cartItem.product.discountedPrice
-                            : cartItem.product.price}
+                        {cartItem.product.isDiscount &&
+                        cartItem.product.discountedPrice
+                            ? cartItem.product.discountedPrice +
+                              colorPrice +
+                              optionPrice
+                            : cartItem.product.price + colorPrice + optionPrice}
                     </h5>
                     <button onClick={handleDeleteProductFromCart}>
                         <IoCloseOutline className="text-3xl" />
