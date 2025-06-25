c5bfefd774148057c9773410f32328676fec58ee
LakioLive <mr.lakio.sergii@gmail.com>
2025-06-25 22:05:14 +0200

    feat: Added redirectToCheckoutAction, deleteProductsFromCartAction and updateOrderAfterCheckoutAction, minor changes to actions in cartActions

--- frontend/src/data/actions/cartActions.ts
+++ frontend/src/data/actions/cartActions.ts
@@ -1,16 +1,17 @@
 "use server";
 
 import qs from "qs";
+import { redirect } from "next/navigation";
 import { mutateData } from "../services/mutateData";
 import { getUserMeLoader } from "../services/getUserMeLoader";
-import { getCartProductData } from "../loaders";
+import { getCartProductData, getCartProductsData } from "../loaders";
+import { ICart } from "@/interfaces/interfaces";
 
 export async function addQuantityToProductAction(documentId: string) {
-    const cartData = await getCartProductData(documentId);
-    const productCart = cartData[0];
+    const productCart = await getCartProductData(documentId);
     const user = await getUserMeLoader();
 
-    if (!cartData.length) {
+    if (!productCart) {
         throw new Error("Product not found");
     }
 
@@ -49,11 +50,10 @@ export async function addQuantityToProductAction(documentId: string) {
 }
 
 export async function removeQuantityToProductAction(documentId: string) {
-    const cartData = await getCartProductData(documentId);
-    const productCart = cartData[0];
+    const productCart = await getCartProductData(documentId);
     const user = await getUserMeLoader();
 
-    if (!cartData.length) {
+    if (!productCart) {
         throw new Error("Product not found");
     }
 
@@ -96,11 +96,10 @@ export async function removeQuantityToProductAction(documentId: string) {
 }
 
 export async function deleteProductFromCartAction(documentId: string) {
-    const cartData = await getCartProductData(documentId);
-    const productCart = cartData[0];
+    const productCart = await getCartProductData(documentId);
     const user = await getUserMeLoader();
 
-    if (!cartData.length) {
+    if (!productCart) {
         throw new Error("Product not found");
     }
 
@@ -120,5 +119,104 @@ export async function deleteProductFromCartAction(documentId: string) {
     if (responseData.error) {
         console.error(responseData.error);
         throw new Error("Failed to Delete Product from Cart.");
+    }
+}
+
+export async function redirectToCheckoutAction(cartItemsData: ICart[]) {
+    const user = await getUserMeLoader();
+
+    if (!user.ok) {
+        throw new Error("User not found");
+    }
+
+    const query = qs.stringify({
+        populate: "*",
+    });
+
+    const payload = {
+        data: {
+            cartItems: cartItemsData,
+        },
+    };
+
+    const responseData = await mutateData(
+        "POST",
+        `/api/orders?${query}`,
+        payload,
+    );
+
+    if (!responseData) {
+        throw new Error("Ops! Something went wrong. Please try again.");
+    }
+
+    if (responseData.error) {
+        console.error(responseData.error);
+        throw new Error("Failed to Redirect to Checkout");
+    }
+
+    const url = responseData.stripeSession.url;
+
+    redirect(url);
+}
+
+export async function deleteProductsFromCartAction() {
+    const cartData: ICart[] = await getCartProductsData();
+    const user = await getUserMeLoader();
+
+    if (!cartData.length) {
+        throw new Error("Products not found");
+    }
+
+    if (!user.ok) {
+        throw new Error("User not found");
+    }
+
+    for (const productCart of cartData) {
+        const responseData = await mutateData(
+            "DELETE",
+            `/api/carts/${productCart.documentId}`,
+        );
+
+        if (!responseData) {
+            throw new Error("Ops! Something went wrong. Please try again.");
+        }
+
+        if (responseData.error) {
+            console.error(responseData.error);
+            throw new Error("Failed to Delete Products from Cart.");
+        }
+    }
+}
+
+export async function updateOrderAfterCheckoutAction(isSuccess: boolean) {
+    const user = await getUserMeLoader();
+
+    if (!user.ok) {
+        throw new Error("User not found");
+    }
+
+    const query = qs.stringify({
+        populate: "*",
+    });
+
+    const payload = {
+        data: {
+            isSuccess: isSuccess,
+        },
+    };
+
+    const responseData = await mutateData(
+        "POST",
+        `/api/orders/update-order-after-checkout?${query}`,
+        payload,
+    );
+
+    if (!responseData) {
+        throw new Error("Ops! Something went wrong. Please try again.");
+    }
+
+    if (responseData.error) {
+        console.error(responseData.error);
+        throw new Error("Failed to Redirect to Checkout");
     }
 }
