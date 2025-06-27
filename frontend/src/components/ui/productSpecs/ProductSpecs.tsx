884b6a85f70f518603d6c79000f8edd74270a052
LakioLive <mr.lakio.sergii@gmail.com>
2025-06-27 22:56:43 +0200

    feat: Added new icons and updated styles in ProductSpecs component

--- frontend/src/components/ui/productSpecs/ProductSpecs.tsx
+++ frontend/src/components/ui/productSpecs/ProductSpecs.tsx
@@ -1,12 +1,18 @@
 import { IconType } from "react-icons";
-import { MdOutlineSmartphone, MdOutlineCamera, MdCamera } from "react-icons/md";
+import {
+    MdOutlineSmartphone,
+    MdOutlineCamera,
+    MdCamera,
+    MdPhotoCameraBack,
+} from "react-icons/md";
 import { GoCpu } from "react-icons/go";
-import { IoBatteryFull } from "react-icons/io5";
+import { IoBatteryFull, IoVideocamOutline } from "react-icons/io5";
 import { FaEye, FaHeadphones } from "react-icons/fa";
-import { RiRefreshLine, RiWeightLine } from "react-icons/ri";
+import { RiRefreshLine, RiWeightLine, RiCameraLensLine } from "react-icons/ri";
 import { IoIosLaptop } from "react-icons/io";
 import { GrStorage } from "react-icons/gr";
 import { PiUserSound } from "react-icons/pi";
+import { TbPhotoSensor, TbFocusAuto } from "react-icons/tb";
 import { ProductSpecsProps } from "./ProductSpecs.interfaces";
 
 const initialIconData: { [key: string]: IconType } = {
@@ -22,6 +28,11 @@ const initialIconData: { [key: string]: IconType } = {
     storage: GrStorage,
     headphone: FaHeadphones,
     noise: PiUserSound,
+    sensor: TbPhotoSensor,
+    iso: RiCameraLensLine,
+    autofocus: TbFocusAuto,
+    resolution: IoVideocamOutline,
+    screen: MdPhotoCameraBack,
 };
 
 export default function ProductSpecs({
@@ -37,10 +48,16 @@ export default function ProductSpecs({
                         key={spec.id}
                         className="flex items-center gap-2 p-3 bg-gray-100 rounded-xl"
                     >
-                        <Icon className="text-2xl" />
+                        <div>
+                            <Icon className="text-xl md:text-2xl" />
+                        </div>
                         <div className="grid">
-                            <span className="text-gray-500">{spec.name}</span>
-                            <span>{spec.specification}</span>
+                            <span className="text-sm md:text-base text-gray-500">
+                                {spec.name}
+                            </span>
+                            <span className="text-xs md:text-sm">
+                                {spec.specification}
+                            </span>
                         </div>
                     </div>
                 );
