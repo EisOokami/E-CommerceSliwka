cf7fd0d01c713673287441f9060137908290f5f5
LakioLive <mr.lakio.sergii@gmail.com>
2025-06-29 14:49:11 +0200

    feat: Added AuthInput component

---
+++ frontend/src/components/ui/authInput/AuthInput.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+import { AuthInputProps } from "./AuthInput.interfaces";
+
+import { ZodErrors } from "../zodErrors/ZodErrors";
+
+export default function AuthInput({
+    defaultValue = "",
+    zodErrorMsg,
+    id,
+    name,
+    type = "text",
+    labelValue,
+    containerClassName = "",
+    inputClassName = "",
+    labelClassName = "",
+}: Readonly<AuthInputProps>) {
+    return (
+        <div className={twMerge(`relative ${containerClassName}`)}>
+            <input
+                className={twMerge(
+                    `w-full px-4 pt-5 pb-4 text-gray-700 border rounded-lg appearance-none leading-tight focus:outline-none peer ${inputClassName}`,
+                )}
+                id={id}
+                name={name}
+                type={type}
+                defaultValue={defaultValue || ""}
+                placeholder=" "
+            />
+            <label
+                className={twMerge(
+                    `absolute left-4 pt-1 text-sm text-gray-700 font-bold transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:left-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:left-4 peer-focus:text-sm peer-focus:text-gray-700 ${labelClassName}`,
+                )}
+                htmlFor={id}
+            >
+                {labelValue}
+            </label>
+            <ZodErrors error={zodErrorMsg} />
+        </div>
+    );
+}
