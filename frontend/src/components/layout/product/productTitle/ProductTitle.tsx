fe7a5eabc2c96bca253314f2c5992695533bfa77
LakioLive <mr.lakio.sergii@gmail.com>
2025-07-01 23:27:25 +0200

    feat: Updated ProductTitle component

--- frontend/src/components/layout/product/productTitle/ProductTitle.tsx
+++ frontend/src/components/layout/product/productTitle/ProductTitle.tsx
@@ -2,10 +2,6 @@ import { ProductTitleProps } from "./ProductTitle.interfaces";
 
 export default function ProductTitle({ title }: Readonly<ProductTitleProps>) {
     return (
-        <div>
-            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
-                {title}
-            </h1>
-        </div>
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">{title}</h1>
     );
 }
