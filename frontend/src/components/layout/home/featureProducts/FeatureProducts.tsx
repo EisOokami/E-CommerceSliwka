f42a8d51d04d6db89643e742260cd449b3d9408b
LakioLive <mr.lakio.sergii@gmail.com>
2025-07-02 18:48:06 +0200

    feat: Updated design in FeatureProducts component

--- frontend/src/components/layout/home/featureProducts/FeatureProducts.tsx
+++ frontend/src/components/layout/home/featureProducts/FeatureProducts.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
+import { getStrapiMedia } from "@/lib/utils";
+import { LuArrowUpRight } from "react-icons/lu";
 import { FeatureProductsProps } from "./FeatureProducts.interfaces";
-
-import StrapiImage from "@/components/ui/strapiImage/StrapiImage";
-import Button from "@/components/ui/button/Button";
 
 export default function FeatureProducts({
     data,
@@ -12,139 +11,109 @@ export default function FeatureProducts({
     const { featureProduct } = data;
 
     return (
-        <section className="grid md:grid-cols-[25%_25%_50%] md:grid-rows-3 xl:grid-rows-2">
+        <section className="grid grid-cols-12 grid-rows-[repeat(10,_minmax(0,_100px))] sm:grid-rows-12 gap-3 md:gap-8 container sm:h-screen mx-auto px-5 py-10 md:py-20">
             <Link
                 href={`catalog/${
                     featureProduct[0].product
-                        ? featureProduct[0].product.documentId
+                        ? featureProduct[0].product.slug
                         : ""
                 }`}
-                className="md:col-span-2 md:row-span-1 grid md:flex justify-items-center md:justify-items-start md:items-center md:gap-5 p-5 md:pl-0"
+                className="relative col-span-12 sm:col-span-7 lg:col-span-5 row-span-2 sm:row-span-4 lg:row-span-7 grid content-end p-8 bg-no-repeat bg-cover bg-center rounded-lg shadow-[inset_0px_-150px_50px_0px_rgba(0,_0,_0,_0.45)] hover:scale-105 transition"
+                style={{
+                    backgroundImage: `url(${getStrapiMedia(
+                        featureProduct[0].image.url,
+                    )})`,
+                }}
             >
-                <StrapiImage
-                    src={featureProduct[0].image.url}
-                    alt={
-                        featureProduct[0].image.alternativeText ??
-                        featureProduct[0].heading
-                    }
-                    width={250}
-                    height={200}
-                    className="md:object-scale-down xl:object-cover md:object-right md:w-40 md:h-full"
-                />
-                <div className="text-center md:text-left grid gap-5">
-                    <h3 className="text-4xl md:text-3xl xl:text-6xl font-medium">
-                        {featureProduct[0].heading}
-                    </h3>
-                    <p className="text-gray-700">
-                        {featureProduct[0].subHeading}
-                    </p>
-                </div>
+                <LuArrowUpRight className="absolute top-2 right-2 text-3xl md:text-4xl lg:text-5xl text-white" />
+                <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-light text-white">
+                    {featureProduct[0].subHeading}
+                </h1>
             </Link>
             <Link
                 href={`catalog/${
                     featureProduct[1].product
-                        ? featureProduct[1].product.documentId
+                        ? featureProduct[1].product.slug
                         : ""
                 }`}
-                className="md:col-span-1 md:row-span-3 xl:row-span-2 relative grid md:flex justify-items-center md:justify-items-start md:justify-between md:items-center order-last md:order-none p-5 md:p-0 md:pl-16 bg-[#EDEDED] overflow-hidden"
+                className="relative col-span-12 sm:col-span-5 lg:col-span-3 row-span-2 sm:row-span-4 lg:row-span-7 grid content-end p-5 bg-no-repeat bg-contain bg-center bg-gray-200 rounded-lg shadow-[inset_0px_-150px_50px_0px_rgba(255,_255,_255,_0.45)] hover:scale-105 transition"
+                style={{
+                    backgroundImage: `url(${
+                        process.env.NEXT_PUBLIC_DB_URL +
+                        featureProduct[1].image.url
+                    })`,
+                }}
             >
-                <div className="text-center md:text-left grid justify-items-center md:justify-items-start gap-5">
-                    <h3 className="text-4xl md:text-5xl lg:text-7xl font-light">
-                        {featureProduct[1].heading
-                            .split(" ")
-                            .slice(0, -1)
-                            .join(" ")}{" "}
-                        <span className="font-semibold">
-                            {featureProduct[1].heading.split(" ").pop()}
-                        </span>
-                    </h3>
-                    <p className="text-gray-700">
-                        {featureProduct[1].subHeading}
-                    </p>
-                    <Button
-                        // href={featureProduct[1].product.documentId}
-                        theme="dark"
-                        text="Shop now"
-                        inline
-                        // isLink
-                    />
-                </div>
-                <StrapiImage
-                    src={featureProduct[1].image.url}
-                    alt={
-                        featureProduct[1].image.alternativeText ??
-                        featureProduct[1].heading
-                    }
-                    width={800}
-                    height={800}
-                    className="md:object-cover md:object-left order-first md:order-none w-96 md:w-auto h-full"
-                />
+                <LuArrowUpRight className="absolute top-2 right-2 text-3xl md:text-4xl lg:text-5xl" />
+                <h2 className="text-lg md:text-2xl lg:text-3xl font-medium">
+                    {featureProduct[1].heading}
+                </h2>
+                <h3 className="text-lg md:text-xl lg:text-2xl">
+                    {featureProduct[1].subHeading}
+                </h3>
             </Link>
             <Link
                 href={`catalog/${
                     featureProduct[2].product
-                        ? featureProduct[2].product.documentId
+                        ? featureProduct[2].product.slug
                         : ""
                 }`}
-                className="md:col-span-2 xl:col-span-1 md:row-span-1 grid md:flex justify-items-center md:items-center md:gap-4 p-5 md:p-0 md:pr-5 xl:pr-10 bg-[#EDEDED]"
+                className="relative col-span-12 lg:col-span-4 row-span-2 sm:row-span-4 lg:row-span-12 grid content-end p-5 bg-no-repeat bg-cover bg-center rounded-lg shadow-[inset_0px_-200px_50px_0px_rgba(0,_0,_0,_0.45)] hover:scale-105 transition"
+                style={{
+                    backgroundImage: `url(${
+                        process.env.NEXT_PUBLIC_DB_URL +
+                        featureProduct[2].image.url
+                    })`,
+                }}
             >
-                <StrapiImage
-                    src={featureProduct[2].image.url}
-                    alt={
-                        featureProduct[2].image.alternativeText ??
-                        featureProduct[2].heading
-                    }
-                    width={250}
-                    height={300}
-                    className="md:object-none md:object-right md:w-48 md:h-full"
-                />
-                <div className="text-center md:text-left grid gap-5">
-                    <h4 className="text-4xl md:text-2xl lg:text-3xl">
-                        {featureProduct[2].heading
-                            .split(" ")
-                            .slice(0, -1)
-                            .join(" ")}{" "}
-                        <span className="font-semibold">
-                            {featureProduct[2].heading.split(" ").pop()}
-                        </span>
-                    </h4>
-                    <p className="text-gray-700">
-                        {featureProduct[2].subHeading}
-                    </p>
-                </div>
+                <LuArrowUpRight className="absolute top-2 right-2 text-3xl md:text-4xl lg:text-5xl text-white" />
+                <h2 className="text-xl md:text-2xl lg:text-3xl text-white font-medium">
+                    {featureProduct[2].heading}
+                </h2>
+                <h3 className="text-xs sm:text-base md:text-lg lg:text-xl text-white">
+                    {featureProduct[2].subHeading}
+                </h3>
             </Link>
             <Link
                 href={`catalog/${
                     featureProduct[3].product
-                        ? featureProduct[3].product.documentId
+                        ? featureProduct[3].product.slug
                         : ""
                 }`}
-                className="md:col-span-2 xl:col-span-1 md:row-span-1 grid md:flex justify-items-center md:items-center md:gap-4 p-5 md:p-0 md:pr-5 xl:pr-10 bg-[#353535]"
+                className="relative col-span-12 sm:col-span-5 lg:col-span-3 row-span-2 sm:row-span-4 lg:row-span-5 grid content-end p-5 bg-no-repeat bg-contain bg-center bg-gray-200 rounded-lg shadow-[inset_0px_-75px_50px_0px_rgba(255,_255,_255,_0.45)] hover:scale-105 transition"
+                style={{
+                    backgroundImage: `url(${
+                        process.env.NEXT_PUBLIC_DB_URL +
+                        featureProduct[3].image.url
+                    })`,
+                }}
             >
-                <StrapiImage
-                    src={featureProduct[3].image.url}
-                    alt={
-                        featureProduct[3].image.alternativeText ??
-                        featureProduct[3].heading
-                    }
-                    width={300}
-                    height={300}
-                    className="md:object-none md:object-right md:w-48 xl:w-36 md:h-full xl:h-52"
-                />
-                <div className="text-center md:text-left grid gap-5">
-                    <h4 className="text-4xl md:text-2xl lg:text-3xl text-white">
-                        {featureProduct[3].heading
-                            .split(" ")
-                            .slice(0, -1)
-                            .join(" ")}{" "}
-                        <span className="font-semibold">
-                            {featureProduct[3].heading.split(" ").pop()}
-                        </span>
-                    </h4>
-                    <p className="text-gray-400">
-                        {featureProduct[3].subHeading}
-                    </p>
-                </div>
+                <LuArrowUpRight className="absolute top-2 right-2 text-3xl md:text-4xl lg:text-5xl" />
+                <h2 className="text-xl md:text-2xl font-medium">
+                    {featureProduct[3].heading}
+                </h2>
+            </Link>
+            <Link
+                href={`catalog/${
+                    featureProduct[4].product
+                        ? featureProduct[4].product.slug
+                        : ""
+                }`}
+                className="relative col-span-12 sm:col-span-7 lg:col-span-5 row-span-2 sm:row-span-4 lg:row-span-5 grid content-between p-5 bg-no-repeat bg-cover bg-center rounded-lg shadow-[inset_0px_75px_50px_-35px_rgba(0,_0,_0,_0.45)] hover:scale-105 transition"
+                style={{
+                    backgroundImage: `url(${
+                        process.env.NEXT_PUBLIC_DB_URL +
+                        featureProduct[4].image.url
+                    })`,
+                }}
+            >
+                <LuArrowUpRight className="absolute top-2 right-2 text-3xl md:text-4xl lg:text-5xl text-white" />
+                <h3 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-white drop-shadow-2xl">
+                    {featureProduct[4].subHeading}
+                </h3>
+                <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-white">
+                    {featureProduct[4].heading}
+                </h2>
             </Link>
         </section>
     );
