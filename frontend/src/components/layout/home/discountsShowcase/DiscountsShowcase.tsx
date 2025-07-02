8547a22711822709b1d520a9763f15e848873817
LakioLive <mr.lakio.sergii@gmail.com>
2025-07-02 18:50:58 +0200

    feat: Updated styles and added CardsComponent instead of Card in DiscountsShowcase component

--- frontend/src/components/layout/home/discountsShowcase/DiscountsShowcase.tsx
+++ frontend/src/components/layout/home/discountsShowcase/DiscountsShowcase.tsx
@@ -1,6 +1,6 @@
 import { DiscountsShowcaseProps } from "./DiscountsShowcase.interfaces";
 
-import Card from "@/components/ui/card/Card";
+import CardsComponent from "@/components/ui/card/CardsComponent";
 
 export default function DiscountsShowcase({
     data,
@@ -10,32 +10,14 @@ export default function DiscountsShowcase({
     const { products } = data;
 
     return (
-        <section className="container mx-auto px-3 md:px-5 py-10">
+        <section className="container mx-auto px-5 py-10">
             <div className="mb-8">
                 <h4 className="text-2xl md:text-3xl font-medium">
                     Discounts uр to -50%
                 </h4>
             </div>
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-                {products.map((product) => (
-                    <Card
-                        key={product.documentId}
-                        imageSrc={`${process.env.NEXT_PUBLIC_DB_URL}${product.image.url}`}
-                        imageAlt={product.name}
-                        imageWidth={250}
-                        imageHeight={250}
-                        title={product.name}
-                        price={`$${
-                            product.isDiscount
-                                ? product.discountedPrice
-                                : product.price
-                        }`}
-                        buttonHref={`/catalog/${product.documentId}`}
-                        buttonTheme="dark"
-                        buttonText="Buy Now"
-                        buttonClassName="self-end"
-                    />
-                ))}
+                <CardsComponent productsData={products} />
             </div>
         </section>
     );
