c5c606a040c6148450e7a8745276c59ac123803f
LakioLive <mr.lakio.sergii@gmail.com>
2025-07-06 20:34:21 +0200

    feat: Updated catalog page

--- frontend/src/app/catalog/page.tsx
+++ frontend/src/app/catalog/page.tsx
@@ -1,38 +1,49 @@
 "use client";
 
-import { Suspense, useState } from "react";
+import { useEffect, useState } from "react";
+import useCatalogStore from "@/stores/catalog";
+import { usePathname } from "next/navigation";
+import { IProduct } from "@/interfaces/interfaces";
 
 import Breadcrumb from "@/components/ui/breadcrumb/Breadcrumb";
-import Catalog from "@/components/layout/catalog/Catalog";
 import Pagination from "@/components/ui/pagination/Pagination";
-
-const limit = 8;
+import AccordionContainer from "@/components/layout/catalog/accordionContainer/AccordionContainer";
+import Products from "@/components/layout/catalog/products/Products";
+import RatingSelect from "@/components/ui/ratingSelect/RatingSelect";
 
 export default function CatalogPage() {
-    const [totalPages, setTotalPages] = useState<number>(1);
-    const [currentPage, setCurrentPage] = useState<number>(1);
-    const [productsCount, setProductsCount] = useState<number>(0);
+    const setSearchValue = useCatalogStore((state) => state.setSearchValue);
+    const setProductRating = useCatalogStore((state) => state.setProductRating);
+    const setCurrentPage = useCatalogStore((state) => state.setCurrentPage);
+    const [filteredProducts, setFilteredProducts] = useState<IProduct[]>([]);
+    const pathname = usePathname();
+
+    useEffect(() => {
+        if (pathname === "/catalog") {
+            setProductRating("all");
+            setSearchValue("");
+            setCurrentPage(1);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [pathname]);
 
     return (
         <>
-            <section className="container mx-auto px-3 md:px-5">
+            <section className="container mx-auto px-5">
                 <Breadcrumb />
             </section>
-            <Suspense fallback={<p>Loading products...</p>}>
-                <Catalog
-                    limit={limit}
-                    totalPages={totalPages}
-                    setTotalPages={setTotalPages}
-                    currentPage={currentPage}
-                    productsCount={productsCount}
-                    setProductsCount={setProductsCount}
-                />
-            </Suspense>
-            <Pagination
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
-                totalPages={totalPages}
-            />
+            <section className="grid md:flex md:items-start gap-5 container mx-auto px-5">
+                <div className="md:self-start flex md:block justify-between items-center gap-5 md:w-1/3">
+                    <AccordionContainer
+                        setFilteredProducts={setFilteredProducts}
+                    />
+                    <div className="block md:hidden w-full">
+                        <RatingSelect />
+                    </div>
+                </div>
+                <Products productsData={filteredProducts} />
+            </section>
+            <Pagination />
         </>
     );
 }
