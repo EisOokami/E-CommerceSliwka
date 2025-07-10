e697612be4f25938629fc165b604f1272c736549
LakioLive <mr.lakio.sergii@gmail.com>
2025-07-10 22:20:47 +0200

    feat: Updated cart page

--- frontend/src/app/cart/page.tsx
+++ frontend/src/app/cart/page.tsx
@@ -1,16 +1,17 @@
 import { getCartProductsData } from "@/data/loaders";
 import { IoCartOutline } from "react-icons/io5";
+import { ICart } from "@/interfaces/interfaces";
 
 import CartList from "@/components/layout/cart/cartList/CartList";
 import OrderSummary from "@/components/layout/cart/orderSummary/OrderSummary";
 
 export default async function CartPage() {
-    const data = await getCartProductsData();
+    const data: ICart[] = await getCartProductsData();
 
     return (
-        <main className="container mx-auto px-3 md:px-5 py-10 md:py-20">
-            <div className="flex items-end gap-1">
-                <IoCartOutline className="text-3xl" />
+        <main className="container mx-auto px-5 py-10 md:py-20">
+            <div className="flex items-center gap-1">
+                <IoCartOutline className="md:mt-1 text-3xl" />
                 <h1 className="text-2xl md:text-3xl xl:text-4xl font-medium">
                     Shopping Cart
                 </h1>
