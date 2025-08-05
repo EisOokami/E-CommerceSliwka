4c270f70ec447b3466c6d31acd8139eba3e73d0a
EisOokami <mr.lakio.sergii@gmail.com>
2025-08-05 17:41:17 +0200

    feat: Added Content Security Policy

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -25,7 +25,47 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL("/", request.url));
     }
 
-    return NextResponse.next();
+    const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+    const cspHeader = `
+            default-src 'self';
+            script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+            style-src 'self' 'nonce-${nonce}';
+            style-src-attr 'self' 'unsafe-inline';
+            img-src 'self' http://127.0.0.1:1337;
+            connect-src 'self' http://127.0.0.1:1337;
+            font-src 'self';
+            object-src 'none';
+            base-uri 'self';
+            form-action 'self';
+            frame-ancestors 'none';
+            upgrade-insecure-requests;
+        `
+        .replace(/\s{2,}/g, " ")
+        .trim();
+
+    const contentSecurityPolicyHeaderValue = cspHeader
+        .replace(/\s{2,}/g, " ")
+        .trim();
+
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set("x-nonce", nonce);
+
+    requestHeaders.set(
+        "Content-Security-Policy",
+        contentSecurityPolicyHeaderValue,
+    );
+
+    const response = NextResponse.next({
+        request: {
+            headers: requestHeaders,
+        },
+    });
+    response.headers.set(
+        "Content-Security-Policy",
+        contentSecurityPolicyHeaderValue,
+    );
+
+    return response;
 }
 
 export const config = {
@@ -37,6 +77,12 @@ export const config = {
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
          */
-        "/((?!api|_next/static|_next/image|favicon.ico).*)",
+        {
+            source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+            missing: [
+                { type: "header", key: "next-router-prefetch" },
+                { type: "header", key: "purpose", value: "prefetch" },
+            ],
+        },
     ],
 };
