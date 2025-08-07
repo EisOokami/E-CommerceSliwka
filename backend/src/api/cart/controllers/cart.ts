77907e77967e627fc7f8c69cfb5f383028324ab1
EisOokami <mr.lakio.sergii@gmail.com>
2025-08-07 21:30:20 +0200

    feat: Updated core services in cart and updated cart schema

--- backend/src/api/cart/controllers/cart.ts
+++ backend/src/api/cart/controllers/cart.ts
@@ -2,16 +2,101 @@
  * cart controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::cart.cart', {
-    async cartsCount(ctx) {
-        const user = ctx.state.user
+export default factories.createCoreController("api::cart.cart", {
+    async find(ctx) {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.unauthorized("You are not authorized!");
+        }
+
+        const params = (await this.sanitizeQuery(ctx)) as Record<string, any>;
 
         const result = await strapi
             .service("api::cart.cart")
-            .getCount(user);
+            .findCartProductsData(params, user);
 
         ctx.send(result);
-    }
+    },
+
+    async findOne(ctx) {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.unauthorized("You are not authorized!");
+        }
+
+        const params = (await this.sanitizeQuery(ctx)) as Record<string, any>;
+
+        const result = await strapi
+            .service("api::cart.cart")
+            .findCartProductData(params, user, ctx.params);
+
+        ctx.send(result);
+    },
+
+    async create(ctx) {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.unauthorized("You are not authorized!");
+        }
+
+        const params = (await this.sanitizeQuery(ctx)) as Record<string, any>;
+
+        const result = await strapi
+            .service("api::cart.cart")
+            .createCartProductData(params, user, ctx.request.body.data);
+
+        ctx.send(result);
+    },
+
+    async update(ctx) {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.unauthorized("You are not authorized!");
+        }
+
+        const params = (await this.sanitizeQuery(ctx)) as Record<string, any>;
+
+        const result = await strapi
+            .service("api::cart.cart")
+            .updateCartProductData(
+                params,
+                user,
+                ctx.request.body.data,
+                ctx.params,
+            );
+
+        ctx.send(result);
+    },
+
+    async delete(ctx) {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.unauthorized("You are not authorized!");
+        }
+
+        const result = await strapi
+            .service("api::cart.cart")
+            .deleteCartProductData(user, ctx.params);
+
+        ctx.send(result);
+    },
+
+    async cartsCount(ctx) {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.unauthorized("You are not authorized!");
+        }
+
+        const result = await strapi.service("api::cart.cart").getCount(user);
+
+        ctx.send(result);
+    },
 });
--- backend/src/api/cart/services/cart.ts
+++ backend/src/api/cart/services/cart.ts
@@ -2,22 +2,132 @@
  * cart service
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreService('api::cart.cart', ({ strapi }) => ({
-    async getCount(user) {
-        const count = user ? await strapi.documents("api::cart.cart").count({
-            populate: "*",
+export default factories.createCoreService("api::cart.cart", ({ strapi }) => ({
+    async findCartProductsData(params, user) {
+        const { results, pagination } = await super.find(params);
+
+        const cartData = await strapi.documents("api::cart.cart").findMany({
+            populate: {
+                ...params.populate,
+                user: {
+                    fields: ["id"],
+                },
+            },
+            filters: {
+                ...params.filters,
+                user: {
+                    documentId: {
+                        $eq: user.documentId,
+                    },
+                },
+            },
+            sort: params.sort,
+        });
+
+        return { data: cartData, meta: { pagination } };
+    },
+
+    async findCartProductData(params, user, ctxParams) {
+        const cartData = await strapi.documents("api::cart.cart").findOne({
+            documentId: ctxParams.id,
+            populate: {
+                ...params.populate,
+                user: {
+                    fields: ["id"],
+                },
+            },
+            filters: {
+                ...params.filters,
+                user: {
+                    documentId: {
+                        $eq: user.documentId,
+                    },
+                },
+            },
+            sort: params.sort,
+        });
+
+        return { data: cartData };
+    },
+
+    async createCartProductData(params, user, data) {
+        const cartData = await strapi.documents("api::cart.cart").create({
+            data: {
+                ...data,
+                user: user.id,
+            },
+            populate: {
+                ...params.populate,
+                user: {
+                    fields: ["id"],
+                },
+            },
             status: "published",
-            filters: { 
-                user: { 
-                    id: { 
-                        $eq: user.id 
-                    } 
-                } 
-            } 
-        }) : 0;
+        });
 
-        return {data: count}
-    }
+        return { data: cartData };
+    },
+
+    async updateCartProductData(params, user, data, ctxParams) {
+        const cartData = await strapi.documents("api::cart.cart").update({
+            documentId: ctxParams.id,
+            data: {
+                ...data,
+                user: user.id,
+            },
+            populate: {
+                ...params.populate,
+                user: {
+                    fields: ["id"],
+                },
+            },
+            filters: {
+                ...params.filters,
+                user: {
+                    documentId: {
+                        $eq: user.documentId,
+                    },
+                },
+            },
+            sort: params.sort,
+            status: "published",
+        });
+
+        return { data: cartData };
+    },
+
+    async deleteCartProductData(user, ctxParams) {
+        await strapi.documents("api::cart.cart").delete({
+            documentId: ctxParams.id,
+            filters: {
+                user: {
+                    documentId: {
+                        $eq: user.documentId,
+                    },
+                },
+            },
+        });
+
+        return { ok: true };
+    },
+
+    async getCount(user) {
+        const count = user
+            ? await strapi.documents("api::cart.cart").count({
+                  populate: "*",
+                  status: "published",
+                  filters: {
+                      user: {
+                          id: {
+                              $eq: user.id,
+                          },
+                      },
+                  },
+              })
+            : 0;
+
+        return { data: count };
+    },
 }));
