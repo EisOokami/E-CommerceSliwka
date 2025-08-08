edcfeec9989921c38f2181a7890b317f11fad8a2
EisOokami <mr.lakio.sergii@gmail.com>
2025-08-08 15:50:41 +0200

    feat: Updated core services and controllers in wishlist, and updated wishlist schema

--- backend/src/api/wishlist/controllers/wishlist.ts
+++ backend/src/api/wishlist/controllers/wishlist.ts
@@ -2,16 +2,62 @@
  * wishlist controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::wishlist.wishlist', {
+export default factories.createCoreController("api::wishlist.wishlist", {
+    async find(ctx) {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.unauthorized("You are not authorized!");
+        }
+
+        const params = (await this.sanitizeQuery(ctx)) as Record<string, any>;
+
+        const result = await strapi
+            .service("api::wishlist.wishlist")
+            .findWishlistProductsData(params, user);
+
+        ctx.send(result);
+    },
+
+    async create(ctx) {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.unauthorized("You are not authorized!");
+        }
+
+        const params = (await this.sanitizeQuery(ctx)) as Record<string, any>;
+
+        const result = await strapi
+            .service("api::wishlist.wishlist")
+            .createWishlistProductData(params, user, ctx.request.body.data);
+
+        ctx.send(result);
+    },
+
+    async delete(ctx) {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.unauthorized("You are not authorized!");
+        }
+
+        const result = await strapi
+            .service("api::wishlist.wishlist")
+            .deleteWishlistProductData(user, ctx.params);
+
+        ctx.send(result);
+    },
+
     async wishlistsCount(ctx) {
-        const user = ctx.state.user
+        const user = ctx.state.user;
 
         const result = await strapi
             .service("api::wishlist.wishlist")
             .getCount(user);
 
         ctx.send(result);
-    }
+    },
 });
--- backend/src/api/wishlist/services/wishlist.ts
+++ backend/src/api/wishlist/services/wishlist.ts
@@ -2,22 +2,88 @@
  * wishlist service
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreService('api::wishlist.wishlist', ({ strapi }) => ({
-    async getCount(user) {
-        const count = user ? await strapi.documents("api::wishlist.wishlist").count({
-            populate: "*",
-            status: "published",
-            filters: { 
-                user: { 
-                    id: { 
-                        $eq: user.id 
-                    } 
-                } 
-            } 
-        }) : 0;
+export default factories.createCoreService(
+    "api::wishlist.wishlist",
+    ({ strapi }) => ({
+        async findWishlistProductsData(params, user) {
+            const { results, pagination } = await super.find(params);
 
-        return {data: count}
-    }
-}));
+            const wishlistData = await strapi
+                .documents("api::wishlist.wishlist")
+                .findMany({
+                    populate: {
+                        ...params.populate,
+                        user: {
+                            fields: ["id"],
+                        },
+                    },
+                    filters: {
+                        ...params.filters,
+                        user: {
+                            documentId: {
+                                $eq: user.documentId,
+                            },
+                        },
+                    },
+                    sort: params.sort,
+                });
+
+            return { data: wishlistData, meta: { pagination } };
+        },
+
+        async createWishlistProductData(params, user, data) {
+            const wishlistData = await strapi
+                .documents("api::wishlist.wishlist")
+                .create({
+                    data: {
+                        ...data,
+                        user: user.id,
+                    },
+                    populate: {
+                        ...params.populate,
+                        user: {
+                            fields: ["id"],
+                        },
+                    },
+                    status: "published",
+                });
+
+            return { data: wishlistData };
+        },
+
+        async deleteWishlistProductData(user, ctxParams) {
+            await strapi.documents("api::wishlist.wishlist").delete({
+                documentId: ctxParams.id,
+                filters: {
+                    user: {
+                        documentId: {
+                            $eq: user.documentId,
+                        },
+                    },
+                },
+            });
+
+            return { ok: true };
+        },
+
+        async getCount(user) {
+            const count = user
+                ? await strapi.documents("api::wishlist.wishlist").count({
+                      populate: "*",
+                      status: "published",
+                      filters: {
+                          user: {
+                              id: {
+                                  $eq: user.id,
+                              },
+                          },
+                      },
+                  })
+                : 0;
+
+            return { data: count };
+        },
+    }),
+);
