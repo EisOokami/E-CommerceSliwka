9558d45bbb69d190dc963ef78a207d0a68223498
EisOokami <mr.lakio.sergii@gmail.com>
2025-08-08 15:49:38 +0200

    feat: Updated core service and controllers in product

--- backend/src/api/product/controllers/product.ts
+++ backend/src/api/product/controllers/product.ts
@@ -2,35 +2,51 @@
  * product controller
  */
 
-import { factories } from "@strapi/strapi"
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController("api::product.product", ({ strapi }) => ({
-    async priceRange(ctx) {
-        const filters = ctx.query.filters || {};
-        const result = await strapi
-            .service("api::product.product")
-            .getPriceRange(filters);
+export default factories.createCoreController(
+    "api::product.product",
+    ({ strapi }) => ({
+        async find(ctx) {
+            const params = (await this.sanitizeQuery(ctx)) as Record<
+                string,
+                any
+            >;
 
-        ctx.send(result);
-    },
+            const result = await strapi
+                .service("api::product.product")
+                .findProducts(params);
 
-    async filtered(ctx) {
-        const filters = ctx.query.filters || {};
-        const queryParams = ctx.query.queryParams;
-        const pagination = ctx.query.pagination;
+            ctx.send(result);
+        },
 
-        const result = await strapi
-            .service("api::product.product")
-            .getFilteredProducts(filters, queryParams, pagination);
+        async priceRange(ctx) {
+            const filters = ctx.query.filters || {};
+            const result = await strapi
+                .service("api::product.product")
+                .getPriceRange(filters);
 
-        ctx.send(result);
-    },
+            ctx.send(result);
+        },
 
-    async productsCount(ctx) {
-        const result = await strapi
-            .service("api::product.product")
-            .getCount();
+        async filtered(ctx) {
+            const filters = ctx.query.filters || {};
+            const queryParams = ctx.query.queryParams;
+            const pagination = ctx.query.pagination;
 
-        ctx.send(result);
-    }
-}));
+            const result = await strapi
+                .service("api::product.product")
+                .getFilteredProducts(filters, queryParams, pagination);
+
+            ctx.send(result);
+        },
+
+        async productsCount(ctx) {
+            const result = await strapi
+                .service("api::product.product")
+                .getCount();
+
+            ctx.send(result);
+        },
+    }),
+);
--- backend/src/api/product/services/product.ts
+++ backend/src/api/product/services/product.ts
@@ -2,80 +2,127 @@
  * product service
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreService("api::product.product", ({ strapi }) => ({
-    async getPriceRange(filters) {
-        const products = await strapi.documents("api::product.product").findMany({
-            filters,
-            fields: ["price", "discountedPrice", "isDiscount"],
-            limit: 1000,
-        });
-    
-        const prices = products.map((product) =>
-            product.isDiscount && product.discountedPrice ? product.discountedPrice : product.price
-        );
-        
-        const minPrice = Math.min(...prices);
-        const maxPrice = Math.max(...prices);
-    
-        return { minPrice, maxPrice };
-    },
+export default factories.createCoreService(
+    "api::product.product",
+    ({ strapi }) => ({
+        async findProducts(params) {
+            const { results, pagination } = await super.find(params);
 
-    async getFilteredProducts(filters, queryParams, pagination) {
-        const updatedFilters = {
-            ...filters,
-            ...(queryParams.price?.length === 2 && {
-                finalPrice: {
-                    $between: [+queryParams.price[0], +queryParams.price[1]],
-                },
-            }),
-        }
+            const productData = await strapi
+                .documents("api::product.product")
+                .findMany({
+                    populate: {
+                        ...params.populate,
+                        reviews: {
+                            populate: {
+                                user: {
+                                    populate: {
+                                        avatar: {
+                                            fields: ["url", "alternativeText"],
+                                        },
+                                    },
+                                    fields: ["username"],
+                                },
+                                product: {
+                                    fields: ["documentId"],
+                                },
+                                images: {
+                                    fields: ["url", "alternativeText"],
+                                },
+                            },
+                            sort: "createdAt:desc",
+                        },
+                    },
+                    filters: params.filters,
+                    sort: params.sort,
+                });
 
-        const populateFields = {
-            image: true,
-            sliderImages: true,
-            colors: true,
-            options: true,
-            productInfo: true,
-            productSpecs: true,
-            category: true,
-            reviews: true,
-            detailedSpecifications: {
-                populate: {
-                    specifications: {
-                        populate: {
-                            specifications: true,
+            return { data: productData, meta: { pagination } };
+        },
+
+        async getPriceRange(filters) {
+            const products = await strapi
+                .documents("api::product.product")
+                .findMany({
+                    filters,
+                    fields: ["price", "discountedPrice", "isDiscount"],
+                    limit: 1000,
+                });
+
+            const prices = products.map((product) =>
+                product.isDiscount && product.discountedPrice
+                    ? product.discountedPrice
+                    : product.price,
+            );
+
+            const minPrice = Math.min(...prices);
+            const maxPrice = Math.max(...prices);
+
+            return { minPrice, maxPrice };
+        },
+
+        async getFilteredProducts(filters, queryParams, pagination) {
+            const updatedFilters = {
+                ...filters,
+                ...(queryParams.price?.length === 2 && {
+                    finalPrice: {
+                        $between: [
+                            +queryParams.price[0],
+                            +queryParams.price[1],
+                        ],
+                    },
+                }),
+            };
+
+            const populateFields = {
+                image: true,
+                sliderImages: true,
+                colors: true,
+                options: true,
+                productInfo: true,
+                productSpecs: true,
+                category: true,
+                reviews: true,
+                detailedSpecifications: {
+                    populate: {
+                        specifications: {
+                            populate: {
+                                specifications: true,
+                            },
                         },
                     },
                 },
-            },
-        };
+            };
 
-        const products = await strapi.documents("api::product.product").findMany({
-            filters: updatedFilters,
-            populate: populateFields,
-            start: (pagination.page - 1) * pagination.pageSize,
-            limit: pagination.pageSize
-        });
+            const products = await strapi
+                .documents("api::product.product")
+                .findMany({
+                    filters: updatedFilters,
+                    populate: populateFields,
+                    start: (pagination.page - 1) * pagination.pageSize,
+                    limit: pagination.pageSize,
+                });
 
-        const count = await strapi.documents("api::product.product").count({
-            filters: updatedFilters,
-            populate: "*",
-            status: "published"
-        });
+            const count = await strapi.documents("api::product.product").count({
+                filters: updatedFilters,
+                populate: "*",
+                status: "published",
+            });
 
-        const totalPages = Math.ceil(count / pagination.pageSize)
-    
-        return {data: products, totalPages, productsCount: count };
-    },
+            const totalPages = Math.ceil(count / pagination.pageSize);
 
-    async getCount() {
-        const count = await strapi.documents("api::product.product").count({
-            populate: "*",
-            status: "published"
-        });
+            return { data: products, totalPages, productsCount: count };
+        },
 
-        return {data: count}
-    }
-}))
+        async getCount() {
+            const count = await strapi.documents("api::product.product").count({
+                populate: "*",
+                status: "published",
+            });
+
+            return { data: count };
+        },
+    }),
+);
